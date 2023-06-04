0f1094294f7ce3a161d54f6591d1eefc5833604c
jagjo <pirsingh6283@gmail.com>
2023-06-04 14:53:25 +0530

    Initial commit

---
+++ DClickAnimation/src/main/java/com/dhindsadeveloper/dclickanimation/DClickAnimation.java
@@ -0,0 +1,15 @@
+package com.dhindsadeveloper.dclickanimation;
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+
+public class DClickAnimation {
+    public void clickAnimation(final View view) {
+
+        ScaleAnimation fade_in = new ScaleAnimation(0.9f, 1f, 0.9f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.7f);
+        fade_in.setDuration(300);
+        fade_in.setFillAfter(true);
+        view.startAnimation(fade_in);
+    }
+}
